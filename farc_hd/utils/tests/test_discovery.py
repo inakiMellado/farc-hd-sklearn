fc35ae484e030698507c6690ee38f32c616e5353
Inaki Mellado <mellado.149322@e.unavarra.es>
2026-04-15 11:44:37 +0200

    Remove old template tests and update discovery counts for FARC-HD

--- farc_hd/utils/tests/test_discovery.py
+++ farc_hd/utils/tests/test_discovery.py
@@ -3,29 +3,22 @@
 
 import pytest
 
-from farc_hd.utils.discovery import all_displays, all_estimators, all_functions
-
+from farc_hd.utils.discovery import all_displays, all_estimators
 
 def test_all_estimators():
     estimators = all_estimators()
-    assert len(estimators) == 3
+    assert len(estimators) == 1  # Solo tenemos FarcHDClassifier
 
     estimators = all_estimators(type_filter="classifier")
     assert len(estimators) == 1
 
     estimators = all_estimators(type_filter=["classifier", "transformer"])
-    assert len(estimators) == 2
+    assert len(estimators) == 1
 
     err_msg = "Parameter type_filter must be"
     with pytest.raises(ValueError, match=err_msg):
         all_estimators(type_filter="xxxx")
 
-
 def test_all_displays():
     displays = all_displays()
     assert len(displays) == 0
-
-
-def test_all_functions():
-    functions = all_functions()
-    assert len(functions) == 3
